19748138f98272d07f90ba275a20ae5f906bc1a0
MikitaBelikau <mikita87@tut.by>
2018-06-26 21:27:48 +0300

    Insertion Sort implementation

--- Sorting/Sorting/Program.cs
+++ Sorting/Sorting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Sorting
 {
@@ -6,7 +7,49 @@ namespace Sorting
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			List<int> unsortedList = new List<int>() { 9, 1, 9, 3, 7, 0, 6, 2, 4, 5, -1, 8, 1, 9};
+
+
+			Insertion_Sort(unsortedList);
 		}
+
+		private static void Insertion_Sort(List<int> arr)
+		{
+
+			/**********************/
+			Console.WriteLine("Unsorted list");
+
+			foreach (var item in arr)
+			{
+				Console.WriteLine(item);
+			}
+			/***********************/
+
+			for (int j = 1; j < arr.Count; j++)
+			{
+				int key = arr[j];
+
+				int i = j - 1;
+
+				while (i >= 0 && arr[i] > key)
+				{
+					int temp = arr[i + 1];
+					arr[i + 1] = arr[i];
+					arr[i] = temp;
+					i = i - 1;
+				}
+			}
+
+			/**********************/
+			Console.WriteLine("Sorted list");
+
+			foreach (var item in arr)
+			{
+				Console.WriteLine(item);
+			}
+			/***********************/
+		
+		}
+
 	}
 }
