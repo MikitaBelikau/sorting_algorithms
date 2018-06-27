cb2a013d1e1b051db76a3da107e5c384a4f3d75a
MikitaBelikau <mikita87@tut.by>
2018-06-27 17:54:09 +0300

    added reverse order in insertion sorting

--- Sorting/Sorting/Program.cs
+++ Sorting/Sorting/Program.cs
@@ -10,7 +10,7 @@ namespace Sorting
 			List<int> unsortedList = new List<int>() { 9, 1, 9, 3, 7, 0, 6, 2, 4, 5, -1, 8, 1, 9};
 
 
-			Insertion_Sort(unsortedList);
+			Insertion_Sort_Reverse(unsortedList);
 		}
 
 		private static void Insertion_Sort(List<int> arr)
@@ -46,5 +46,37 @@ namespace Sorting
 		
 		}
 
-	}
+        private static void Insertion_Sort_Reverse(List<int> arr)
+        {
+            /**********************/
+            Console.WriteLine("Unsorted list");
+            foreach (var item in arr)
+            { Console.WriteLine(item); }
+            /***********************/
+
+
+            for (int j = 1; j < arr.Count; j++)
+            {
+                int key = arr[j];
+                int i = j - 1;
+
+                while (i >= 0 && arr[i] < key)
+                {
+                    arr[i + 1] = arr[i];
+                    i = i - 1;
+                }
+                arr[i + 1] = key;
+            }
+
+
+            /**********************/
+            Console.WriteLine("Sorted list");
+            foreach (var item in arr)
+            { Console.WriteLine(item); }
+            /***********************/
+
+            Console.ReadLine();
+
+        }
+    }
 }
